d19db8acb5e8d8e98f0eda8590ca63ce7273fccf
KrishSure <40511366+KrishSure@users.noreply.github.com>
2018-09-16 15:43:58 +0530

    Update App.java
    Updated from remote Repository

--- GitSampleProject/src/main/java/org/maven/git/GitSampleProject/App.java
+++ GitSampleProject/src/main/java/org/maven/git/GitSampleProject/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //Remote Repository change
         System.out.println( "Hello World!" );
         System.out.println( "Welcome to Git World" );
     }
